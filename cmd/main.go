bbbf6ffd5ad560aa8dd1fee9cecc48369fc67316
Zy654321cao <SGchuyue@qq.com>
2020-10-24 12:42:20 +0800

    sl

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,21 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"github.com/SGchuyue/logger/logger"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"net"
-	pb "watch_etcd/proto"
-	s "watch_etcd/server"
+	r "watch_etcd/server"
 )
 
 type service struct{}
 
-func (s *service) SendTest(ctx context.Context, req *pb.TestRequest) (res *pb.TestResponse, err error) {
-	fmt.Printf("邮箱:%s;发送内容:%s", req.Send, req.Text)
-	return &pb.TestResponse{
-		Ok: true,
-	}, nil
-}
-
 func main() {
-	// 监听本地的8972端口
+	logger.InitLogger("test.log", 1, 1, 7, false)
+	// 监听本地的8080端口
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Printf("failed to listen: %v", err)
+		logger.Error("监听时出现问题", err)
 		return
 	}
 	gs := grpc.NewServer() // 创建gRPC服务器
@@ -31,16 +23,16 @@ func main() {
 	reflection.Register(gs) //在给定的gRPC服务器上注册服务器反射服务
 	// Serve方法在lis上接受传入连接，为每个连接创建一个ServerTransport和server的goroutine。
 	// 该goroutine读取gRPC请求，然后调用已注册的处理程序来响应它们。
-	//etcd服务注册
-	reg := s.NewService(s.Info{
-		Name: "etcdtest",
-		IP:   "127.0.0.1:645", // grpc服务节点ip
-	}, []string{"127.0.0.1:2379", "127.0.0.1:2279", "127.0.0.1:3379"}) // etcd的节点ip
-	go reg.Run()
-	gk := reg.GetValue()
-	go reg.Watch(gk)
 	if err := gs.Serve(lis); err != nil {
-		fmt.Println(err)
+		logger.Error("调用服务发生错误:", err)
 	}
-	fmt.Println(222)
+	// etcd服务注册
+	etcd := r.NewService(r.Info{
+		Name: "etcdtest",
+		IP:   "127.0.0.1:645",
+		Type: "DEBUG",
+	}, []string{"127.0.0.1:4339", "127.0.0.1:2299", "127.0.0.1:1776"}) // etcd的节点ip
+	etcd.Run()
+	gk := etcd.GetValue()
+	go etcd.Watch(gk)
 }
